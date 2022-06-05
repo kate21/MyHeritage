a7fb7555cfc2b3f73b7f8456657968dcd3393e9c
unknown <katrina21.07@gmail.com>
2022-06-05 20:15:24 +0300

    correct result saved in file + wait

--- MyHeritage/src/main/java/Main.java
+++ MyHeritage/src/main/java/Main.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.time.Duration;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-
 import org.apache.commons.io.FileUtils;
 
 public class Main {
@@ -17,6 +17,7 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:\\test scripts\\tools\\chromedriver_win32\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://google.com");
 
         driver.findElement(By.xpath("//*[text()=\"Прийняти\"]")).click();
@@ -26,7 +27,7 @@ public class Main {
         driver.findElement(By.xpath("//div[@class=\"get_dna_test_button\"]")).click();
 
         String priceoffer = driver.findElement(By.className("offer_price")).getText();
-        //String text = "Price = ";
+        String text = "Price = € ";
         //System.out.println(priceoffer);
 
         Pattern pt = Pattern.compile("\\d+(\\\\.\\\\d+)?");
@@ -35,18 +36,18 @@ public class Main {
             System.out.println(mt.group());
             String price = mt.group();
 
+            String result = text + price;
+
+
             File f = new File("savetxt.txt");
             try {
-                FileUtils.writeStringToFile(f, price, Charset.defaultCharset());
+                FileUtils.writeStringToFile(f, result, Charset.defaultCharset());
             } catch (IOException exc) {
                 exc.printStackTrace();
             }
 
-
-//end
-
         }
     }
-
 }
+
 
