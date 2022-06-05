a4f7565ff93e02eb1d797ce2fa335d07d2cf5fc2
unknown <katrina21.07@gmail.com>
2022-06-05 19:50:55 +0300

    Separate sum in file

--- MyHeritage/src/main/java/Main.java
+++ MyHeritage/src/main/java/Main.java
@@ -3,25 +3,50 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import org.apache.commons.io.FileUtils;
 
 public class Main {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\test scripts\\tools\\chromedriver_win32\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
+
         driver.findElement(By.xpath("//*[text()=\"Прийняти\"]")).click();
         WebElement element = driver.findElement(By.xpath("//input[@class=\"gLFyf gsfi\"]"));
         element.sendKeys("My Heritage", Keys.ENTER);
         driver.findElement(By.xpath("//div[@class=\"g tF2Cxc\"]")).click();
         driver.findElement(By.xpath("//div[@class=\"get_dna_test_button\"]")).click();
-        String text = driver.findElement(By.className("offer_price")).getText();
-        System.out.println(text);
+
+        String priceoffer = driver.findElement(By.className("offer_price")).getText();
+        //String text = "Price = ";
+        //System.out.println(priceoffer);
+
+        Pattern pt = Pattern.compile("\\d+(\\\\.\\\\d+)?");
+        Matcher mt = pt.matcher(priceoffer);
+        if (mt.find()) {
+            System.out.println(mt.group());
+            String price = mt.group();
+
+            File f = new File("savetxt.txt");
+            try {
+                FileUtils.writeStringToFile(f, price, Charset.defaultCharset());
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            }
 
 
 //end
 
+        }
     }
 
-
 }
+
